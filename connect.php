5aa8d664d86171e87357ca9145ba8414dcfb976c
huynh23trh <tranghttpc02468@fpt.edu.vn>
2023-09-19 16:52:31 +0700

    [P3.1 video2] - connect DB

--- connect.php
+++ connect.php
@@ -1,14 +1,10 @@
 <?php
-// $servername = "localhost";
-// $username = "root";
-// $password = "mysql";
-
-$servername = "sql.freedb.tech";
-$username = "freedb_php1_test";
-$password = "3ppdxa8WD?NNQyB";
+$servername = "localhost";
+$username = "root";
+$password = "";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=freedb_php1_test", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=duanmau_x_shop", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
