df2be9f5f1390d5f519c5616c16973430c27cc5c
huynh23trh <tranghttpc02468@fpt.edu.vn>
2023-09-22 03:28:36 +0700

    [P4.2] - Biến toàn cục global

---
+++ global.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Định nghĩa các url cần thiết được sử dụng trong website
+ */
+$ROOT_URL = '/DuanMautranghttpc02468';
+$CONTENT_URL = "$ROOT_URL/content";
+$ADMIN_URL = "$ROOT_URL/admin";
+$SITE_URL = "$ROOT_URL/site";
