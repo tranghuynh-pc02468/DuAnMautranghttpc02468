21aa37b325d2c8d776f9a67da67294fa15e3af95
huynh23trh <tranghttpc02468@fpt.edu.vn>
2023-09-30 15:38:52 +0700

    layout trang chính admin

--- admin/menu.php
+++ admin/menu.php
@@ -44,30 +44,10 @@
                                     Dashboard
                                     <i class="right fas fa-angle-left"></i>
                                 </p>
-                            </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="./index.html" class="nav-link active">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Dashboard v1</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="./index2.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Dashboard v2</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="./index3.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Dashboard v3</p>
-                                    </a>
-                                </li>
-                            </ul>
+                            </a> 
                         </li>
                         <li class="nav-item">
-                            <a href="pages/widgets.html" class="nav-link">
+                            <a href="../loai-hang/" class="nav-link">
                                 <i class="nav-icon fas fa-th"></i>
                                 <p>
                                     Loại hàng
