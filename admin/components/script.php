91a237425e3962b692ad8b17ccb4cb514d8e586d
huynh23trh <tranghttpc02468@fpt.edu.vn>
2023-09-30 15:38:11 +0700

    chia layout Admin

---
+++ admin/components/script.php
@@ -0,0 +1,34 @@
+  <!-- jQuery -->
+  <script src="<?=$CONTENT_URL?>/plugins/jquery/jquery.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="<?=$CONTENT_URL?>/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
+        $.widget.bridge('uibutton', $.ui.button)
+    </script>
+    <!-- Bootstrap 4 -->
+    <script src="<?=$CONTENT_URL?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- ChartJS -->
+    <script src="<?=$CONTENT_URL?>/plugins/chart.js/Chart.min.js"></script>
+    <!-- Sparkline -->
+    <script src="<?=$CONTENT_URL?>/plugins/sparklines/sparkline.js"></script>
+    <!-- JQVMap -->
+    <script src="<?=$CONTENT_URL?>/plugins/jqvmap/jquery.vmap.min.js"></script>
+    <script src="<?=$CONTENT_URL?>/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+    <!-- jQuery Knob Chart -->
+    <script src="<?=$CONTENT_URL?>/plugins/jquery-knob/jquery.knob.min.js"></script>
+    <!-- daterangepicker -->
+    <script src="<?=$CONTENT_URL?>/plugins/moment/moment.min.js"></script>
+    <script src="<?=$CONTENT_URL?>/plugins/daterangepicker/daterangepicker.js"></script>
+    <!-- Tempusdominus Bootstrap 4 -->
+    <script src="<?=$CONTENT_URL?>/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <!-- Summernote -->
+    <script src="<?=$CONTENT_URL?>/plugins/summernote/summernote-bs4.min.js"></script>
+    <!-- overlayScrollbars -->
+    <script src="<?=$CONTENT_URL?>/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?=$CONTENT_URL?>/dist/js/adminlte.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="<?=$CONTENT_URL?>/dist/js/demo.js"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="<?=$CONTENT_URL?>/dist/js/pages/dashboard.js"></script>
