20b2de09f1dfd4c6a1cf0db9e083488b229b61e0
huynh23trh <tranghttpc02468@fpt.edu.vn>
2023-09-30 15:32:19 +0700

    [P4.4] - hàm exit_param

--- global.php
+++ global.php
@@ -6,3 +6,7 @@ $ROOT_URL = '/DuanMautranghttpc02468';
 $CONTENT_URL = "$ROOT_URL/content";
 $ADMIN_URL = "$ROOT_URL/admin";
 $SITE_URL = "$ROOT_URL/site";
+
+function exit_param($fieldname){
+    return array_key_exists($fieldname, $_REQUEST);
+}
