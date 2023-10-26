4a69db5197c33cf6f6504b824b0234949255ea01
huynh23trh <tranghttpc02468@fpt.edu.vn>
2023-10-26 20:35:40 +0700

    [admin] - count hh kh

--- admin/trang-chinh/index.php
+++ admin/trang-chinh/index.php
@@ -1,6 +1,10 @@
 <?php
     require "../../global.php";
+    require "../../dao/hang-hoa.php";
+    require "../../dao/khach-hang.php";
 
+    $item = hang_hoa_count();
+    $kh_count = khach_hang_count();
     $VIEW_NAME="trang-chinh/home.php";
     
     require "../layout.php";
